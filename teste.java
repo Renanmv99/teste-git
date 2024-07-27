563533e250040dfe7e9589d9bc03e0182ac6fc91
Renan Vicente <renanvicente@gmail.com>
2024-07-26 21:15:36 -0300

    teste token

--- teste.java
+++ teste.java
@@ -2,6 +2,7 @@ public class teste {
     public static void main(String[] args) {
         System.out.println("TESTE DA BRANCH");
         System.out.println("TESTE BRANCH ISSUE");
+        System.out.println("teste token");
 
     }
 }
